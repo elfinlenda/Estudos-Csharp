128d7d55d27cb2f39a66523f8461cfe4ed68e4a5
elfinlenda <fabioemanuelcna@gmail.com>
2026-09-03 19:15:24 +0100

    atividade 2

---
+++ Exercicios   C# - POO/Atividade2/Aluno.cs
@@ -0,0 +1,66 @@
+class Aluno
+{
+    // atributos do aluno
+    // praticando manualmente get e set
+    private string _nome;
+    public string Nome
+    {
+        get{ return _nome;}
+        set
+        {
+            _nome = value;
+        }
+    }
+
+    private double _nota1;
+    public double Nota1
+    {
+        get{ return _nota1;}
+        set
+        {   // logica p/ evitar a nota erraada
+            if(value < 0 && value > 10)
+
+                throw new ArgumentException(" valor de nota1 invalido!!");        
+                _nota1 = value;
+        }
+    }
+
+    private double _nota2;
+    public double Nota2
+    {
+        get{ return _nota2;}
+        set
+        {   // logica p/ evitar a nota erraada
+            if(value < 0 && value > 10)
+            
+                throw new ArgumentException(" valor de noda2 invalido!!");
+                _nota2 = value;
+        }
+    }
+
+
+// METODOS DE ALUNO
+    private double Media()
+    {
+         return (Nota1 + Nota2)/2 ;
+    }
+
+    private string Situacao(double media)
+    {
+        if(media < 6 )
+        {
+           return "REPROVADO";
+        }
+        else
+        {
+            return "APROVADO";
+        }
+    }
+
+    public void Mensagem()
+    {
+        double retorno = Media();
+        string retorno2 = Situacao(retorno);
+        Console.WriteLine($"INFORMACOES DO ALUNO\nNome:{Nome}\nMedia:{retorno}\nSituacao{retorno2}");
+    }
+}
---
+++ Exercicios   C# - POO/Atividade2/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+- Atividade: Criar uma classe Aluno, contendo os atributos:
+  a) nome
+  b) nota1
+  c) nota2
+
+Haverá três métodos, sendo dois privados e um público:
+Media: Método privado para retornar a média do aluno.
+Situacao: Método privado para retornar se está aprovado ou reprovado.
+Mensagem: Método publico para retornar o nome, média e situação.
+
+- O que vamos utilizar: Classe, atributo, método, objeto e encapsulamento
+
+*/
+
+Aluno aluno1 = new();
+
+aluno1.Nome = "Fabio emanuel";
+aluno1.Nota1 = 4.98;
+aluno1.Nota2 = 5.5;
+
+
+
+aluno1.Mensagem();
