fb01c6319825487757dc915b936e28867ff09fe9
elfinlenda <fabioemanuelcna@gmail.com>
2026-09-03 21:51:47 +0100

    atividade 4

---
+++ Exercicios   C# - POO/Atividade4/Media.cs
@@ -0,0 +1,14 @@
+class Media
+{
+    public Media(double num1, double num2)
+    {
+        double media = (num1 + num2)/2 ;
+        Console.WriteLine($"media de 2: {media}\n");
+    }
+    public Media(double num1, double num2, double num3)
+    {
+        double media = (num1 + num2 + num3)/3 ;
+        Console.WriteLine($"media de 3: {media}\n");
+    }
+
+}
---
+++ Exercicios   C# - POO/Atividade4/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+EXERCÍCIO 04
+
+- Atividade: Criar uma classe Media, que não terá atributos.
+
+Haverá dois construtores, onde um terá duas notas como parâmetros
+e outro construtor terá três notas como parâmetros.
+Ambos os construtores precisam exibir o valor da média via Console.
+
+- O que vamos utilizar: Classe, objeto,
+construtor e polimorfismo (overloading/sobrecarga)
+*/
+
+Media media1 = new(3,11);
+Media media2 = new(3,11,5);
