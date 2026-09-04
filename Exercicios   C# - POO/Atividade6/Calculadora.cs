fc8eced294f25e21ff453e6a42fda8dcb4231d7e
elfinlenda <fabioemanuelcna@gmail.com>
2026-09-04 02:03:54 +0100

    atividade 6

---
+++ Exercicios   C# - POO/Atividade6/Calculadora.cs
@@ -0,0 +1,14 @@
+class Calculadora
+{
+    // metodos apenas levando em conta o enunciado
+
+    public static void somar(int num1, int num2)
+    {
+        Console.WriteLine($"A soma dos numeros e {num1 + num2}");
+    }
+    public static void subtrair(int num1, int num2)
+    {
+        Console.WriteLine($"A soma dos numeros e {num1 - num2}");
+    }
+
+}
---
+++ Exercicios   C# - POO/Atividade6/Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+
+EXERCÍCIO 06
+
+- Atividade: Crie uma calculadora contendo os métodos: Somar e Subtrair.
+Ambos os métodos terão dois parâmetros inteiros.
+Os métodos deverão ser estáticos.
+
+- O que vamos utilizar: Classe, objeto, atributo e método estático
+*/
+
+Calculadora.somar(12,12);
+Calculadora.subtrair(1,12);
